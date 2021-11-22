8495a6eb96d7fa006ce2bb33dd3bcfc389e05c6d
Asyraf Rahim <asyrafboyz@gmail.com>
2021-11-22 12:10:52 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+
+Route::get('client',[ClientController::class,'index'])->name('client');
+Route::get('client/create',[ClientController::class,'create'])->name('client.create');
+Route::post('client/store',[ClientController::class,'store'])->name('client.store');
